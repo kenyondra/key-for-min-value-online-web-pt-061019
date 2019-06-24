dc25a1bb19c742e58e86602eba1ab51f59e803aa
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-24 13:26:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,5 @@ def key_for_min_value(name_hash)
   return nil if name_hash.size == 0 
   min_key, min_value = name_hash.first
   name_hash.each do |key, value|
-    if value < 
+    if value < min_value
 end
